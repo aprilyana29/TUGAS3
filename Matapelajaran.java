4c9d0ae19e46c4e0267a28a7dd70f3b064787802
aprianairawati29@gmail.com <116328033+aprilyana29@users.noreply.github.com>
2022-11-04 21:55:14 +0800

    Add files via upload

---
+++ Matapelajaran.java
@@ -0,0 +1,12 @@
+public class Matapelajaran {
+
+    String kdmt;
+
+    String nmmt;
+
+    final int nilailulus=65;
+
+    void cetakmt(){
+        System.out.println("kode mata pelajaran ="+kdmt+"\n"+"mata pelajaran ="+nmmt+"\n"+"nilai standar lulus="+nilailulus);
+    }
+}
---
+++ Siswa.java
@@ -0,0 +1,24 @@
+public class Siswa {
+    int nis;
+
+    String nama;
+
+    String jkl;
+
+    String alamat;
+
+    public Siswa(){
+    }
+
+    public Siswa(int nis, String nama,String jkl,String alamat){
+        this.nis=nis;
+        this.nama=nama;
+        this.jkl=jkl;
+        this.alamat=alamat;
+    }
+
+    void cetak_siswA(){
+        System.out.println();
+    }
+
+}
